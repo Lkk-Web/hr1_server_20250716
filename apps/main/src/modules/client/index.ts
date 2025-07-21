c7008a39ec562a544ca2fccef619f9db1d927885
醒来就想躺 <workhardcl@icloud.com>
2025-07-21 18:11:51 +0800

    refactor(clear): 客户端相同接口清除

--- apps/main/src/modules/client/index.ts
+++ apps/main/src/modules/client/index.ts
@@ -5,5 +5,4 @@ export * from './productionOrder/productionOrder.module'
 export * from './processTask/processTask.module'
 export * from './productionReport/productionReport.module'
 
-export * from './sysOrg/sysOrg.module'
 
