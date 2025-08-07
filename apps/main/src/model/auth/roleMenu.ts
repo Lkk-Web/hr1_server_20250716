becae00335a1d4ea6324dcd2f53a4610a5f90ea9
LKK-Web <workhardcl@icloud.com>
2025-08-07 18:36:17 +0800

    fix: 角色bugfix & 增加事务

--- apps/main/src/model/auth/roleMenu.ts
+++ apps/main/src/model/auth/roleMenu.ts
@@ -1,9 +1,10 @@
 import { BelongsTo, Column, DataType, ForeignKey, HasMany, Model, Table } from 'sequelize-typescript'
 import { Role } from './role'
 import { Menu } from './menu'
+import { BaseDate } from '@model/shared/baseDate'
 
 @Table({ tableName: `auth_role_menu`, timestamps: false, freezeTableName: true, paranoid: true })
-export class RoleMenu extends Model<RoleMenu> {
+export class RoleMenu extends BaseDate<RoleMenu> {
   @ForeignKey(() => Role)
   @Column({ type: DataType.INTEGER, comment: '角色id' })
   declare roleId: number
