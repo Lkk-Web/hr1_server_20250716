bd8f65f5fb6087d80b977859f1aecaaeb8f2e145
LKK-Web <workhardcl@icloud.com>
2025-08-29 17:33:28 +0800

    fix: 不更新返工的单

--- apps/main/src/modules/station/productionReport/productionReportTwo.service.ts
+++ apps/main/src/modules/station/productionReport/productionReportTwo.service.ts
@@ -68,7 +68,7 @@ export class ProductionReportTwoService {
         for (const item of processDto.positions) {
           console.log(3)
           // 工序任务
-          const processTask = await ProcessTask.findOne({ where: { serialId: item.serialId, processId: process.parentId } })
+          const processTask = await ProcessTask.findOne({ where: { serialId: item.serialId, processId: process.parentId, status: PROCESS_TASK_STATUS.running } })
           await processTask.update(
             {
               status: taskStatus == TaskStatus.OPEN_TASK ? PROCESS_TASK_STATUS.running : PROCESS_TASK_STATUS.pause,
@@ -79,7 +79,7 @@ export class ProductionReportTwoService {
           console.log(4)
           // 工位任务
           const processPositionTask = await ProcessPositionTask.findOne({
-            where: { serialId: item.serialId, processId: dto.processId },
+            where: { serialId: item.serialId, processId: dto.processId, status: POSITION_TASK_STATUS.IN_PROGRESS },
             include: [{ association: 'operateLogs' }],
           })
 
